8c297248c0b176786f944c588e05b4860fd4015b
Hieutrinh1505 <hieutrinh358@gmail.com>
2021-03-16 11:30:27 -0500

    update

--- app.py
+++ app.py
@@ -3,10 +3,8 @@ from flask_cors import cross_origin
 import sklearn
 import pickle
 import pandas as pd
-
 app = Flask(__name__)
 model = pickle.load(open("flight_rf.pkl", "rb"))
-
 
 
 @app.route("/")
